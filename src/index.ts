e9b12968f58437cbf7ba860a74fea1ae34f2ef80
Joel Summerfield <j.summerfield@elifesciences.org>
2019-09-30 16:45:26 +0100

    Cleaned up index.js and the typescript configuration.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { Add, Divide, Multiply, Subtract } from 'experiment.typescript-library';
 
-// tslint:disable-next-line:no-console
-console.log('4 + 2 = ' + Add(4, 2));
+function printMessage(message : string) : void
+{
+    // tslint:disable-next-line:no-console
+    console.log(message);
+}
 
-// tslint:disable-next-line:no-console
-console.log('4 - 2 = ' + Subtract(4, 2));
-
-// tslint:disable-next-line:no-console
-console.log('4 * 2 = ' + Multiply(4, 2));
-
-// tslint:disable-next-line:no-console
-console.log('4 / 2 = ' + Divide(4, 2));
+printMessage('4 + 2 = ' + Add(4, 2));
+printMessage('4 - 2 = ' + Subtract(4, 2));
+printMessage('4 * 2 = ' + Multiply(4, 2));
+printMessage('4 / 2 = ' + Divide(4, 2));
