f33d2e56c8376ca4c1432d161e62c3019f960933
Joel Summerfield <j.summerfield@elifesciences.org>
2019-09-30 15:07:16 +0100

    Initial commit of a very basic binary, note requires experiment.typescript-library via npm link.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Add, Divide, Multiply, Subtract } from 'experiment.typescript-library';
+
+// tslint:disable-next-line:no-console
+console.log('4 + 2 = ' + Add(4, 2));
+
+// tslint:disable-next-line:no-console
+console.log('4 - 2 = ' + Subtract(4, 2));
+
+// tslint:disable-next-line:no-console
+console.log('4 * 2 = ' + Multiply(4, 2));
+
+// tslint:disable-next-line:no-console
+console.log('4 / 2 = ' + Divide(4, 2));
