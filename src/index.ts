53a250c33bae372a0b479abd5b8b900cf159f858
Joel Summerfield <j.summerfield@elifesciences.org>
2019-10-10 17:34:33 +0100

    feat: switched to basic web service
    Switched from a run-once cli to a web service that exposes the library
    functions as post methods on various paths.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,50 @@
 import { Add, Divide, Multiply, Subtract } from '@joelsummerfield/experiment.typescript-library';
+import bodyParser = require('body-parser');
+import { Request, Response, Application } from 'express';
+import express = require('express');
 
-function printMessage(message : string) : void
-{
+const app: Application = express();
+const port = 8080;
+
+app.use(bodyParser.json());
+
+app.post('/add', (req : Request, res : Response) => {
+    const a: number = req.body.a || 0;
+    const b: number = req.body.b || 0;
+    const result = {
+        "result": Add(a, b)
+    };
+    res.status(200).send(JSON.stringify(result));
+});
+
+app.post('/subtract', (req : Request, res : Response) => {
+    const a: number = req.body.a || 0;
+    const b: number = req.body.b || 0;
+    const result = {
+        "result": Subtract(a, b)
+    };
+    res.status(200).send(JSON.stringify(result));
+});
+
+app.post('/multiply', (req : Request, res : Response) => {
+    const a: number = req.body.a || 0;
+    const b: number = req.body.b || 0;
+    const result = {
+        "result": Multiply(a, b)
+    };
+    res.status(200).send(JSON.stringify(result));
+});
+
+app.post('/divide', (req : Request, res : Response) => {
+    const a: number = req.body.a || 0;
+    const b: number = req.body.b || 1;
+    const result = {
+        "result": Divide(a, b)
+    };
+    res.status(200).send(JSON.stringify(result));
+});
+
+app.listen( port, () => {
     // tslint:disable-next-line:no-console
-    console.log(message);
-}
-
-printMessage('4 + 2 = ' + Add(4, 2));
-printMessage('4 - 2 = ' + Subtract(4, 2));
-printMessage('4 * 2 = ' + Multiply(4, 2));
-printMessage('4 / 2 = ' + Divide(4, 2));
+    console.log(`server started at http://localhost:${port}`);
+});
