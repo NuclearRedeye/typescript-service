c4cab3ea86be11b06576f95626e16c2e29635bf0
NuclearRedeye <52923935+NuclearRedeye@users.noreply.github.com>
2020-02-28 15:11:31 +0000

    chore: build a minified version

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,16 @@
+import { terser } from 'rollup-plugin-terser';
 module.exports = [
   {
     input: 'build/index.js',
     output: {
       file: 'dist/bin/typescript-service.js',
     },
+  },
+  {
+    input: 'build/index.js',
+    output: {
+      file: 'dist/bin/typescript-service.min.js',
+    },
+    plugins: [terser()],
   },
 ];
