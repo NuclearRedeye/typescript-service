d1017b9c1d3b8b0a43428a39d78627f4205435fa
NuclearRedeye <52923935+NuclearRedeye@users.noreply.github.com>
2020-02-28 15:12:26 +0000

    chore: move tests to seperate folder

---
+++ test/server.test.ts
@@ -0,0 +1,94 @@
+import request from 'supertest';
+import { app } from '../build/server';
+
+describe('Post /add', () => {
+  it('2 + 2 should be equal to 4', async () => {
+    const res: request.Response = await request(app)
+      .post('/add')
+      .send({
+        a: 2,
+        b: 2,
+      });
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toEqual(4);
+  });
+
+  it('No input should be equal to 0', async () => {
+    const res: request.Response = await request(app)
+      .post('/add')
+      .send({});
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toEqual(0);
+  });
+});
+
+describe('Post /subtract', () => {
+  it('5 - 4 should be equal to 1', async () => {
+    const res: request.Response = await request(app)
+      .post('/subtract')
+      .send({
+        a: 5,
+        b: 4,
+      });
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toEqual(1);
+  });
+
+  it('No input should be equal to 0', async () => {
+    const res: request.Response = await request(app)
+      .post('/subtract')
+      .send({});
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toEqual(0);
+  });
+});
+
+describe('Post /mulitply', () => {
+  it('11 - 3 should be equal to 33', async () => {
+    const res: request.Response = await request(app)
+      .post('/multiply')
+      .send({
+        a: 11,
+        b: 3,
+      });
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toEqual(33);
+  });
+
+  it('No input should be equal to 0', async () => {
+    const res: request.Response = await request(app)
+      .post('/multiply')
+      .send({});
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toEqual(0);
+  });
+});
+
+describe('Post /divide', () => {
+  it('20 - 2 should be equal to 10', async () => {
+    const res: request.Response = await request(app)
+      .post('/divide')
+      .send({
+        a: 20,
+        b: 2,
+      });
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toEqual(10);
+  });
+
+  it('No input should be equal to 0', async () => {
+    const res: request.Response = await request(app)
+      .post('/divide')
+      .send({});
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toEqual(0);
+  });
+});
