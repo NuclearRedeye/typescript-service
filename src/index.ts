49c3bc16293be6d4e9bf0845d25bb24ad30ce188
NuclearRedeye <github@mneah.com>
2021-04-23 16:54:39 +0100

    fix: code styling issues

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ const port = 8080;
 const server: Server = app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 
-  process.on('SIGINT', function() {
-    server.close(function() {
+  process.on('SIGINT', function () {
+    server.close(function () {
       process.exit(0);
     });
   });
--- src/server.ts
+++ src/server.ts
@@ -7,38 +7,38 @@ export const app: Application = express();
 app.use(express.json());
 
 app.post('/add', (req: Request, res: Response) => {
-    console.log('add')
-    const a: number = req.body.a || 0;
-    const b: number = req.body.b || 0;
-    const result = {
-        result: Add(a, b),
-    };
-    res.status(200).json(result);
+  console.log('add');
+  const a: number = req.body.a || 0;
+  const b: number = req.body.b || 0;
+  const result = {
+    result: Add(a, b)
+  };
+  res.status(200).json(result);
 });
 
 app.post('/subtract', (req: Request, res: Response) => {
-    const a: number = req.body.a || 0;
-    const b: number = req.body.b || 0;
-    const result = {
-        result: Subtract(a, b),
-    };
-    res.status(200).json(result);
+  const a: number = req.body.a || 0;
+  const b: number = req.body.b || 0;
+  const result = {
+    result: Subtract(a, b)
+  };
+  res.status(200).json(result);
 });
 
 app.post('/multiply', (req: Request, res: Response) => {
-    const a: number = req.body.a || 0;
-    const b: number = req.body.b || 0;
-    const result = {
-        result: Multiply(a, b),
-    };
-    res.status(200).json(result);
+  const a: number = req.body.a || 0;
+  const b: number = req.body.b || 0;
+  const result = {
+    result: Multiply(a, b)
+  };
+  res.status(200).json(result);
 });
 
 app.post('/divide', (req: Request, res: Response) => {
-    const a: number = req.body.a || 0;
-    const b: number = req.body.b || 1;
-    const result = {
-        result: Divide(a, b),
-    };
-    res.status(200).json(result);
+  const a: number = req.body.a || 0;
+  const b: number = req.body.b || 1;
+  const result = {
+    result: Divide(a, b)
+  };
+  res.status(200).json(result);
 });
